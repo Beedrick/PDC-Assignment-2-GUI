e224b7bb209338c3de16fef17ecd303f46a3293c
Pettric H. Mamea <103038493+Beedrick@users.noreply.github.com>
2023-05-22 23:05:56 +1200

    final
    Done

---
+++ src/CarEngine.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package GUIA2;
+
+/**
+ *
+ * @author Gorilla Rig
+ */
+public class CarEngine {
+    
+    private int number;
+    
+    
+    
+    
+    
+}
---
+++ src/Controller.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package GUIA2;
+
+/**
+ *
+ * @author Gorilla Rig
+ */
+public class Controller {
+    
+}
