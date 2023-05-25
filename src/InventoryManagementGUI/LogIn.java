be9674520c2c25cc3f7f3119846379b869191630
Pettric H. Mamea <103038493+Beedrick@users.noreply.github.com>
2023-05-25 19:51:36 +1200

    Resolved conflict

--- src/InventoryManagementGUI/LogIn.java
+++ src/InventoryManagementGUI/LogIn.java
@@ -12,5 +12,9 @@ public class LogIn {
     
     private int number;
     
+    public static void main(String[] args) {
+        System.out.println("Testing updates resolved conflict");
+    }
+    
     
 }
