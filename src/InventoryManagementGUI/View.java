d8cc26da4040478cd7f92148689790485f8af275
roccomilicic <roccomilicic2@gmail.com>
2023-05-28 19:29:06 +1200

    started working on the login GUI. Still needs a lot of work only the base right now.

--- src/InventoryManagementGUI/View.java
+++ src/InventoryManagementGUI/View.java
@@ -8,6 +8,18 @@ package InventoryManagementGUI;
  *
  * @author Gorilla Rig
  */
+import javax.swing.SwingUtilities;
+
 public class View {
-    
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                LoginController controller = new LoginController();
+                LoginGUI loginGUI = new LoginGUI(controller);
+
+                // Set the LoginGUI as the view in the controller
+                controller.setView(loginGUI);
+            }
+        });
+    }
 }
