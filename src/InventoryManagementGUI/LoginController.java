2e95cdbe6a1627c726417fd0054da4c9cd5462bf
roccomilicic <roccomilicic2@gmail.com>
2023-06-05 23:48:06 +1200

    Modified login classes to adopt correct MVC and cleaned code smell. Login controller now validates the input and determines whether user can login.

--- src/InventoryManagementGUI/LoginController.java
+++ src/InventoryManagementGUI/LoginController.java
@@ -2,28 +2,74 @@ package InventoryManagementGUI;
 
 /**
  *
- * @author rocco
+ * @author rocco + beedrix
  */
-public class LoginController {
 
+import java.sql.*;
+
+public class LoginController {
     private LoginGUI view;
+    private PopupWindow popupWindow;
+    private LogIn model;
+    public static Connection conn;  
+
+    public LoginController() {
+        DatabaseManager dbManager = new DatabaseManager();
+        this.conn = dbManager.getConnection();
+        this.model = new LogIn();
+    }
 
     public void setView(LoginGUI view) {
         this.view = view;
     }
 
-    public void login(String username, String password) {
-        // Perform login logic here
-        // Example: check if username and password are valid
-        if (username.equals("admin") && password.equals("password")) {
-            System.out.println("Login successful");
-            view.dispose();
-            MainMenuGUI userGUI = new MainMenuGUI();
-            userGUI.menuGUI();
+    public boolean validateLogin(String username, String password) {
+    // Checks if the username-password combo is valid
+        boolean valid = false;
+        String query = "SELECT * FROM Users WHERE LOWER(username) = LOWER(?) AND password = ?";
+        ResultSet rs = null;
+        PreparedStatement pstmt = null;
 
-        } else {
-            // Login failed
-            view.displayErrorMessage("Invalid username or password");
+        try { 
+            pstmt = conn.prepareStatement(query);
+            pstmt.setString(1, username);
+            pstmt.setString(2, password);
+
+            rs = pstmt.executeQuery();
+            valid = rs.next();
+
+            return valid;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         }
+        return valid;
+    } 
+
+    public void login(String username, String password) {
+        // Checks for valid username-password match and then allows the user to login
+        if (validateLogin(username, password)) {
+            directToMainMenu();
+        } else {
+            view.resetTextFields();
+            popupWindow = new PopupWindow("Invalid login", "Your username or password is incorrect. \nPlease try again.");
+        }
+    }
+
+    public void directToSignup() {
+        CreateNewUserController createUserController = new CreateNewUserController();
+        CreateNewUserGUI createUserGUI = new CreateNewUserGUI(createUserController);
+        createUserGUI.setVisible(true);
+    }
+    
+    public void directToMainMenu() {
+        
     }
 }
--- src/InventoryManagementGUI/LoginGUI.java
+++ src/InventoryManagementGUI/LoginGUI.java
@@ -1,13 +1,5 @@
 package InventoryManagementGUI;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JPasswordField;
-import javax.swing.JTextField;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -18,44 +10,67 @@ public class LoginGUI extends JFrame {
     private JPasswordField passwordField;
     private JButton loginButton;
     private JButton createUserButton;
-
+    private JLabel titleLabel;
+    private JLabel usernameLabel;
+    private JLabel passwordLabel;
+    private JPanel mainPanel;
+    private JPanel inputPanel;
+    private GridBagConstraints gbc;
+    
     public LoginGUI(LoginController controller) {
         this.controller = controller;
-
+        this.createUserButton = new JButton("Signup");
+        this.loginButton = new JButton("Login");
+        this.titleLabel = new JLabel("Inventory Manager");
+        this.usernameLabel = new JLabel("Username:");
+        this.passwordLabel = new JLabel("Password:");
+        this.mainPanel = new JPanel();
+        this.inputPanel = new JPanel();
+        this.usernameField = new JTextField();
+        this.passwordField = new JPasswordField();
+        this.gbc = new GridBagConstraints();
+        
+        setWindow();
+        createTextField();
+        createButton();
+        createLabel();
+        createPanel();
+        addAllContents();        
+    }
+    
+    public void setWindow() {
         setTitle("Login");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(800, 600);
         setResizable(false);
         setLocationRelativeTo(null);
-
-        JPanel mainPanel = new JPanel();
+    }
+    
+    public void addAllContents() {
         mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
         mainPanel.setBackground(new Color(40, 44, 52));
         mainPanel.setBorder(BorderFactory.createEmptyBorder(50, 50, 50, 50));
 
-        JLabel titleLabel = new JLabel("Login");
-        titleLabel.setFont(new Font("Arial", Font.BOLD, 30));
-        titleLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
-        titleLabel.setForeground(Color.WHITE);
+        mainPanel.add(titleLabel);
+        mainPanel.add(Box.createVerticalStrut(50));
+        mainPanel.add(inputPanel);
+        mainPanel.add(Box.createVerticalStrut(20));
+        mainPanel.add(loginButton);
+        mainPanel.add(Box.createVerticalStrut(10));
+        mainPanel.add(createUserButton);
 
-        JPanel inputPanel = new JPanel();
-        inputPanel.setLayout(new GridBagLayout());
-        inputPanel.setBackground(new Color(51, 55, 64));
-        inputPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+        getContentPane().setBackground(new Color(34, 38, 46));
+        getContentPane().add(mainPanel);
+        setVisible(true);
+    }
 
-        GridBagConstraints gbc = new GridBagConstraints();
+    public void createPanel() {
         gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.insets = new Insets(10, 10, 10, 10);
 
-        JLabel usernameLabel = new JLabel("Username:");
-        usernameLabel.setForeground(Color.WHITE);
-        usernameField = new JTextField();
-        usernameField.setPreferredSize(new Dimension(200, 30));
-
-        JLabel passwordLabel = new JLabel("Password:");
-        passwordLabel.setForeground(Color.WHITE);
-        passwordField = new JPasswordField();
-        passwordField.setPreferredSize(new Dimension(200, 30));
+        inputPanel.setLayout(new GridBagLayout());
+        inputPanel.setBackground(new Color(51, 55, 64));
+        inputPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
 
         gbc.gridx = 0;
         gbc.gridy = 0;
@@ -72,8 +87,23 @@ public class LoginGUI extends JFrame {
         gbc.gridx = 1;
         gbc.gridy = 1;
         inputPanel.add(passwordField, gbc);
+    }
 
-        loginButton = new JButton("Login");
+    public void createLabel() {
+        titleLabel.setFont(new Font("Arial", Font.BOLD, 30));
+        titleLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        titleLabel.setForeground(Color.WHITE);
+
+        usernameLabel.setForeground(Color.WHITE);
+        passwordLabel.setForeground(Color.WHITE);
+
+    }
+    
+    public void createTextField() {
+        usernameField.setPreferredSize(new Dimension(200, 30));
+        passwordField.setPreferredSize(new Dimension(200, 30));
+    }
+    public void createButton() {
         loginButton.setAlignmentX(Component.CENTER_ALIGNMENT);
         loginButton.setBackground(new Color(95, 158, 160));
         loginButton.setForeground(Color.WHITE);
@@ -90,7 +120,6 @@ public class LoginGUI extends JFrame {
             }
         });
 
-        createUserButton = new JButton("Signup");
         createUserButton.setAlignmentX(Component.CENTER_ALIGNMENT);
         createUserButton.setBackground(new Color(52, 152, 219));
         createUserButton.setForeground(Color.WHITE);
@@ -98,24 +127,17 @@ public class LoginGUI extends JFrame {
         createUserButton.setFont(new Font("Arial", Font.BOLD, 16));
         createUserButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                CreateNewUserController createUserController = new CreateNewUserController();
-                CreateNewUserGUI createUserGUI = new CreateNewUserGUI(createUserController);
-                createUserGUI.setVisible(true);
+                // sends user to signup page
                 dispose();
+                controller.directToSignup();
             }
         });
-
-        mainPanel.add(titleLabel);
-        mainPanel.add(Box.createVerticalStrut(50));
-        mainPanel.add(inputPanel);
-        mainPanel.add(Box.createVerticalStrut(20));
-        mainPanel.add(loginButton);
-        mainPanel.add(Box.createVerticalStrut(10));
-        mainPanel.add(createUserButton);
-
-        getContentPane().setBackground(new Color(34, 38, 46));
-        getContentPane().add(mainPanel);
-        setVisible(true);
+    }
+    
+    public void resetTextFields() {
+    // Clears text fields after user has pressed signup/goback
+        usernameField.setText("");
+        passwordField.setText("");
     }
 
     public void displayErrorMessage(String message) {
